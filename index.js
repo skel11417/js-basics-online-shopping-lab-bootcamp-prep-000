8f292f0bc353047c027fefe56ec4ffce78f06c0d
Sean Kelly <shonkelli@gmail.com>
2019-01-30 20:04:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,11 +10,22 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var randPrice = Math.floor(Math.random() * 10) + 1
+  var itemObject = {itemName: item, itemPrice: randPrice}
+  cart.push(itemObject)
+  return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  var outputString = "In your cart, you have "
+  if (cart.length < 1) {
+    return 'Your shopping cart is empty.'
+  } else {
+    for (var i = 0; i < cart.length; i++) {
+      outputString = outputString + `${cart[i]["itemName"]} at $${cart[i]["itemPrice"]}`
+    }
+    return outputString + '.'
+  }
 }
 
 function total() {
